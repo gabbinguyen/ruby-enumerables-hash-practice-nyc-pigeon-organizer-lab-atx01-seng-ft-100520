1dad8485542801ddddbe592b0ab277d14579f468
Gabbi Nguyen <gabbinguyen@gmail.com>
2020-10-05 03:57:49 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,6 +4,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   data.each_with_object({}) do |(key, value), final_array|
   value.each do |new_key, new_value|
+    new_value.each do |new_value|
     binding.pry
   end
   final_array
